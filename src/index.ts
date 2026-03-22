7b67273236f84bd9b303fe081df2630b1e387d45
Eobam <117958786+Eobam@users.noreply.github.com>
2026-03-22 15:42:42 -0400

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { App } from "@slack/bolt";
+import env from "./env";
+
+const app = new App({
+  token: env.WORKSPACE_BOT_TOKEN,
+  signingSecret: env.SIGNING_SECRET,
+});
+const people = env.ALERT_USER_ID.split(",");
+
+app.event("channel_created", async ({ event, client }) => {
+  for (const person of people) {
+    await client.chat.postMessage({
+      channel: person,
+      text: `<#${event.channel.id}> created by <@${event.channel.creator}>`,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `<#${event.channel.id}> (${event.channel.name}) created by <@${event.channel.creator}>`,
+          },
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: `<!subteam^S07MTSEBEK1>`,
+            },
+          ],
+        },
+      ],
+    });
+  }
+});
+
+app.start(3000);
+app.client.chat.postMessage({
+  channel: people[0],
+  text: "wakey wakey",
+});
